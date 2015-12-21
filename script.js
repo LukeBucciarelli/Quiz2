64d4494440edc6a9e5d65a707a8891555bb24a83
fredericJos <fredericaerts@hotmail.com>
2015-12-21 18:36:06 +0100

    mentor update

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
+var questions = [
+  {
+    question: "Who was the first person to climb 5.15?",
+    choices: ["Royal Robbins","Chris Sharma","Tommy Caldwell","Fred Nicole" ],
+    answer: 1
+  },
+  {
+    question: "Name the only 5.15b in North America.",
+    choices: ["Jumbo Love","Chris Sharma","Toxic Turkey","God’s Own Stone" ],
+    answer: 0
+  }
+];
+var choicePrefixes = ["A: ","B: ", "C: ", "D: "];
+var currentQuestion = 0;
 
 
 $(document).ready(function () {
-    
 
-   
-        
-   
+  // get current html elements for later use in click handlers
+  var $choicesListElement = $('.choices-list');
+  var $questionElement = $('.question');
+  var $submitAnswerButton = $('.submit-answer-js');
 
+  // play button click handler
+  $('.play-button-js').click(function () {
+    $questionElement.text(questions[currentQuestion].question);
 
+    var choicesFirstQuestion = questions[currentQuestion].choices;
+    for(var i = 0; i < choicesFirstQuestion.length; i++) {
+      var $listItem = $('<li class="choices-list__item">' + choicePrefixes[i] + choicesFirstQuestion[i] + '</li>');
+      $listItem.click(function() {
+        $('.choices-list').children().removeClass("active");
+        $(this).addClass('active');
+      });
+      $choicesListElement.append($listItem);
+    }
+  });
 
- function quiz() {
-        var scoreAry = [];
-        var questions = [{
-            q: "Q-1: Who was the first person to climb 5.15?"
-            s: ["A.  Royal Robbins","B. Chris Sharma","C. Tommy Caldwell","D.  Fred Nicole" ],
-            a: "B.",
-           
-
-
-
-
-
-
-
-
-
+  // submit answer button click handler
+  $submitAnswerButton.click(function() {
+    $choicesListElement.children().each(function(index) {
+      console.log(index);
     });
+  });
+
+});
